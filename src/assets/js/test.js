d22e3f131055266dace10ecb027b1e132c502a6c
madymadesc <madymadesc@gmail.com>
2022-12-11 18:47:41 +0000

    supression de certaine ligne

--- src/assets/js/test.js
+++ src/assets/js/test.js
@@ -7,19 +7,3 @@ function greetings(name) {
     console.log("Mady Camara");
 
 }
-// c'est un commentaire pour le test
-// function salut(){
-//     alert("salut ogani");
-// }
-
-// function testOne(){
-//     console.log("hello World");
-// }
-
-var x = document.getElementById("myVarX");
-var y = document.getElementById("myVarY");
-function somme(x, y){
-    return x + y;
-}
-
-somme(5,10);
